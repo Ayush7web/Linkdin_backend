0c7f89d09c12de57b4910a1b1a704fc39b008f13
Ayush7web <ayushk93516@gmail.com>
2025-07-27 23:30:58 +0530

    complete

--- controller/user.js
+++ controller/user.js
@@ -17,9 +17,9 @@ exports.googleLogin = async (req, res) => {
     const { token } = req.body;
     const ticket = await client.verifyIdToken({
       idToken: token,
-      aud: process.env.GOOGLE_CLIENT_ID,
+      aud: process.env.GOOGLE_CLIENT_ID, 
     });
-    const payload =  ticket.getPayload();
+    const payload = ticket.getPayload();
 
     const { sub, email, name, picture } = payload;
 
